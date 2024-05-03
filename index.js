4b5e97eaa76ddaee04b6606f3bd57118f8a1c95b
HaiLing Zhang <168756523+trkkz@users.noreply.github.com>
2024-05-03 17:48:09 +0800

    impl async

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
-const main = () => {
-  console.log('main)
+const asyncLimit = (
+  arr,
+  handler,
+  limit = 1,
+  callback = false
+) => {
+  const queue = Array.from(arr)
+  const result = []
+  let count = 0
+  
+  if (limit > arr.length) limit = arr.length
+
+  if (typeof handler !== 'function') {
+    throw new TypeError('handler is not a function')
+  }
+
+  if (typeof callback !== 'function') {
+    console.warn('callback is not a function')
+  }
+
+
+  return new Promise((res, rej) => {
+    const next = () => {
+      if (queue.length <= 0 || count >= limit) return
+      next(count++)
+      handler(queue.shift())
+        .then(data => {
+          next(count--)
+          result.push(data)
+          callback && callback(data, null)
+          count === 0 && res(result)
+        })
+        .catch(err => 
+          callback
+            ? callback(err) 
+            : rej(err)
+        )
+    }
+    
+    next()
+  })
 }
